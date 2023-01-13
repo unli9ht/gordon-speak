f1b5250acf4ca26e77d21077550703cfb5a4597c
Sherwin <74441382+unli9ht@users.noreply.github.com>
2023-01-13 10:05:06 -0800

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,27 +13,27 @@ function convertInputToGordonSpeak() {
      let textInputValue = document.getElementById("text-input").value;
 
      // Splits input into an array, delimited by spaces
-     const textInputArray = textInputValue.split(" ");
+     const wordsToConvertArray = textInputValue.split(" ");
 
      // Create string to display on screen
      let convertedString = "";
-     for (let i = 0; i < textInputArray.length; i++) {
+     for (let i = 0; i < wordsToConvertArray.length; i++) {
           // If the element is only one letter
-          if (textInputArray[i].length == 1) {
-               if (checkIfCapital(textInputArray[i])) {
-                    textInputArray[i] = "O";
+          if (wordsToConvertArray[i].length == 1) {
+               if (checkIfCapital(wordsToConvertArray[i])) {
+                    wordsToConvertArray[i] = "O";
                } else {
-                    textInputArray[i] = "o";
+                    wordsToConvertArray[i] = "o";
                }
           } else {
-               if (checkIfCapital(textInputArray[i])) {
+               if (checkIfCapital(wordsToConvertArray[i])) {
                     //* Strings are immutable in Javascript so have to do it this way
                     let firstChar = "G";
-                    let middleChars = textInputArray[i].substring(1, textInputArray[i].length - 1);
+                    let middleChars = wordsToConvertArray[i].substring(1, wordsToConvertArray[i].length - 1);
                     let lastChar = "";
 
                     // ! Switch - doesn't work?
-                    // switch (checkIfCapital(textInputArray[i].substring(textInputArray[i].length - 1))) {
+                    // switch (checkIfCapital(wordsToConvertArray[i].substring(wordsToConvertArray[i].length - 1))) {
                     //      case true:
                     //           lastChar = "R";
                     //      case false:
@@ -41,29 +41,29 @@ function convertInputToGordonSpeak() {
                     // }
 
                     // ! If - works?
-                    if (checkIfCapital(textInputArray[i].substring(textInputArray[i].length - 1))) {
+                    if (checkIfCapital(wordsToConvertArray[i].substring(wordsToConvertArray[i].length - 1))) {
                          lastChar = "R";
                     } else {
                          lastChar = "r";
                     }
 
-                    textInputArray[i] = firstChar + middleChars + lastChar;
+                    wordsToConvertArray[i] = firstChar + middleChars + lastChar;
                } else {
                     let firstChar = "g";
-                    let middleChars = textInputArray[i].substring(1, textInputArray[i].length - 1);
+                    let middleChars = wordsToConvertArray[i].substring(1, wordsToConvertArray[i].length - 1);
                     let lastChar = "";
 
-                    if (checkIfCapital(textInputArray[i].substring(textInputArray[i].length - 1))) {
+                    if (checkIfCapital(wordsToConvertArray[i].substring(wordsToConvertArray[i].length - 1))) {
                          lastChar = "R";
                     } else {
                          lastChar = "r";
                     }
 
-                    textInputArray[i] = firstChar + middleChars + lastChar;
+                    wordsToConvertArray[i] = firstChar + middleChars + lastChar;
                }
           }
 
-          convertedString += textInputArray[i] + " ";
+          convertedString += wordsToConvertArray[i] + " ";
      }
 
      return convertedString;
@@ -80,8 +80,3 @@ function displayConversion() {
 function checkIfCapital(string) {
      return string.charAt(0) === string.charAt(0).toUpperCase();
 }
-
-let test = "pokemon";
-console.log(test.charAt(0));
-console.log(test.substring(1, test.length - 1));
-console.log(test.charAt(test.length - 1));
